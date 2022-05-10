f5b6d8b555c796c5c5f93f4a6595fa19749ed9da
zhangqian301 <zhangqian301@ke.com>
2022-05-10 11:12:36 +0800

    feat: 添加接口前缀

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,12 @@
 // 封装 router
 import * as Router from 'koa-router';
+import config from '../modules/db/default';
 import user from '../api/controller/user';
 import role from '../api/controller/role';
 
 const router = new Router();
 
-router.use('/user', user.routes(), user.allowedMethods());
-router.use('/role', role.routes(), role.allowedMethods());
+router.use(`${config.api_prefix}/user`, user.routes(), user.allowedMethods());
+router.use(`${config.api_prefix}/role`, role.routes(), role.allowedMethods());
 
 export default router;
